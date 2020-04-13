1ef4eb76b58ea7f01fc42ced48f3085d0de95ea9
Shawn Elston <elstonshawn1@yahoo.com>
2020-04-13 14:09:53 -0700

    football

---
+++ src/App.js
@@ -0,0 +1,51 @@
+//TODO: STEP 1 - Import the useState hook.
+import React, {useState} from "react"; 
+import "./App.css";
+import BottomRow from "./BottomRow";
+
+function App() {
+  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+
+  const [down, setDownScore] = useState(0);
+  const [toGo, setToGoScore] = useState(0);
+  const [homeTeam, setHomeScore] = useState(0);
+  const [awayTeam, setAwayScore] = useState(0);
+  
+   return (
+    <div className="container">
+      <section className="scoreboard">
+        <div className="topRow">
+          <div className="home">
+            <h2 className="home__name">Lions</h2>
+
+            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+
+            <div className="home__score">{homeTeam}</div>
+          </div>
+          <div className="timer">00:03</div>
+          <div className="away">
+            <h2 className="away__name">Tigers</h2>
+            <div className="away__score">{awayTeam}</div>
+          </div>
+        </div>
+        <BottomRow />
+      </section>
+      <section className="buttons">
+        <div className="homeButtons">
+          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          <button onClick={()=> setHomeScore(homeTeam + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={()=> setHomeScore(homeTeam + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+        </div>
+        <div className="awayButtons">
+          <button onClick={()=> setAwayScore(awayTeam + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={()=> setAwayScore(awayTeam + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+        
+          <button onClick={()=> setDownScore(down + 1)} className="homeButtons__touchdown">Home Touchdown</button>
+        
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
